a41644d95500aa335f366569ef5f8aad524f69aa
oliviapoolamae <olivia.poolamae@tktk.ee>
2024-12-09 00:26:05 +0200

    add week14 code

---
+++ Week14OOP/Program.cs
@@ -0,0 +1,63 @@
+﻿
+Console.WriteLine("Name your dog:");
+string myDogName = Console.ReadLine();
+Dog myDog = new Dog(myDogName);
+
+Dog neighboursDog = new Dog("Good Boy");
+
+Console.WriteLine($"My dogs name is {myDog.Name}.");
+Console.WriteLine($"My neighbours dogs name is {neighboursDog.Name}");
+
+myDog.Rename("Bad Boy");
+
+while (myDog.LevelOfHapiness ! = 5)
+{
+    myDog.Bark();
+}
+
+
+myDog.WagTail();
+
+class Dog
+{
+    private string _name; //field
+    private int _levelOfHapiness = 0;
+
+    //constructor
+
+    public Dog(string name) //name - lets the user name the dog
+    {
+        _name = name;
+        _levelOfHapiness = 0;
+    }
+
+    //getter
+
+    public string Name
+    {
+        get { return _name; }
+    }
+
+    public int LevelOfHapiness
+    {
+        get { return _levelOfHapiness; }
+    }
+
+
+    public void Rename(string newName)
+    {
+        _name = newName;
+        Console.WriteLine($"The dog has been renamed to: {newName}");
+    }
+
+    public void Bark()
+    {
+        Console.WriteLine("Woof-Woof!");
+        _levelOfHapiness++;
+    }
+
+    public void WagTail()
+    {
+        Console.WriteLine("Wiggle-Wiggle");
+    }
+}
